34b66ea360a385a74d3c71cd86a86157fb38f783
vicentebrenner <vicentebrenner10@gmail.com>
2025-09-09 22:48:45 -0300

    docker e aws

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./src/routes/authRoutes');
+    const express = require('express');
+    const cors = require('cors');
+    const authRoutes = require('./src/routes/authRoutes');
 
-const app = express();
-const port = 3000;
+    const app = express();
+    const port = 3000;
 
-app.use(cors());
-app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
-app.use('/', authRoutes);
+    app.get('/', (req, res) => {
+      res.status(200).send('<h1>API na AWS está funcionando!</h1><p>Deploy realizado com sucesso.</p>');
+    });
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
+    app.use('/auth', authRoutes);
+
+    app.listen(port, () => {
+        console.log(`Servidor rodando em http://localhost:${port}`);
+    });
