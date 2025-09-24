31b0f9b2a52e0e424e417659512164c467d17e09
vicentebrenner <vicentebrenner10@gmail.com>
2025-09-23 23:04:52 -0300

    testando no postman

--- backend-springboot/Projeto/src/main/java/com/musicMakers/Projeto/ProjetoApplication.java
+++ backend-springboot/Projeto/src/main/java/com/musicMakers/Projeto/ProjetoApplication.java
@@ -2,16 +2,11 @@ package com.musicMakers.Projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.musicMakers.domain.entity")
-@EnableJpaRepositories("com.musicMakers.repository")
 public class ProjetoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoApplication.class, args);
+    }
 }
