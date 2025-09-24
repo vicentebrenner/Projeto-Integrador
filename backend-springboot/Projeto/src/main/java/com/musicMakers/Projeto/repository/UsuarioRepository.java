a1fdbe47b2d2e8ce1a9a9441622854df497914c0
vicentebrenner <vicentebrenner10@gmail.com>
2025-09-23 22:01:43 -0300

    corrigindo bugs

--- backend-springboot/Projeto/src/main/java/com/musicMakers/Projeto/repository/UsuarioRepository.java
+++ backend-springboot/Projeto/src/main/java/com/musicMakers/Projeto/repository/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package com.musicMakers.Projeto.repository;
 
+import com.musicMakers.Projeto.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.musicMakers.Projeto.domain.entity.Usuario;
-
+// A anotação @Repository é opcional, mas recomendada.
+// O mais importante é ser uma INTERFACE que estende JpaRepository.
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
 }
--- backend-springboot/Projeto/src/test/java/com/musicMakers/Projeto/ProjetoApplicationTests.java
+++ backend-springboot/Projeto/src/test/java/com/musicMakers/Projeto/ProjetoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjetoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
